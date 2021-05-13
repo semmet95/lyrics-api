5c9851d384a63169195a76b2b65bb479d77d6eef
amit <singhamitch@gmail.com>
2021-05-13 19:11:12 +0530

    first commit

---
+++ src/main/java/com/semmet/spring/lyricsgeniusapi/LyricsGeniusApiApplication.java
@@ -0,0 +1,13 @@
+package com.semmet.spring.lyricsgeniusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricsGeniusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricsGeniusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semmet/spring/lyricsgeniusapi/LyricsGeniusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semmet.spring.lyricsgeniusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricsGeniusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
